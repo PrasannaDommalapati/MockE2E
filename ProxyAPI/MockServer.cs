717c6ae026c2ce5a5d11cd19d99782cb4b95d8d0
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2022-01-21 13:04:18 +0000

    initial

---
+++ ProxyAPI/MockServer.cs
@@ -0,0 +1,47 @@
+﻿using ProxyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+namespace ProxyAPI
+{
+    public static class MockServer
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public static void RunProxyApi()
+        {
+            var mockServer = WireMockServer.Start(5099);
+
+            Console.WriteLine($"Mock Server started at {mockServer.Urls[0]}");
+
+            var weatherList = SeedWeatherForecasts();
+
+            mockServer
+                .Given(Request.Create().WithPath("/weatherforecast").UsingGet())
+                .RespondWith(Response.Create()
+                .WithStatusCode(HttpStatusCode.OK)
+                .WithBodyAsJson(weatherList));
+        }
+
+        private static IEnumerable<WeatherForecast> SeedWeatherForecasts(int count = 10)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, count).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToList();
+        }
+    }
+
+}
---
+++ StubWebApi/Program.cs
@@ -0,0 +1,31 @@
+﻿using StubWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+namespace StubWebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mockServer = WireMockServer.Start(5099);
+
+            Console.WriteLine($"Mock Server started at {mockServer.Urls[0]}");
+
+            var weatherList = new List<WeatherForecast>();
+
+            mockServer
+                .Given(Request.Create().WithPath("/weatherforecast").UsingGet())
+                .RespondWith(Response.Create().WithStatusCode(HttpStatusCode.OK).WithBody("Hello"));
+
+            Console.ReadKey();
+
+            mockServer.Stop();
+            mockServer.Dispose();
+        }
+    }
+}
